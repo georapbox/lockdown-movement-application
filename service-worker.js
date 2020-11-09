15f5ea6b3cb00dab752ddc70e6c2e4833218ae5d
georapbox <georapbox@gmail.com>
2020-11-09 18:54:29 +0200

    update paths

--- service-worker.js
+++ service-worker.js
@@ -5,9 +5,9 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
-        `index.html`,
-        `styles/main.css`,
-        `scripts/main.js`
+        `./index.html`,
+        `./styles/main.css`,
+        `./scripts/main.js`
       ])
         .then(() => self.skipWaiting())
         .catch(err => console.error(err));
