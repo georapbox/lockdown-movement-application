cb91d6e6423b0d372e353c41a5fa81f534e84c9d
georapbox <georapbox@gmail.com>
2020-11-21 20:08:19 +0200

    dark mode styling

--- service-worker.js
+++ service-worker.js
@@ -9,16 +9,7 @@ self.addEventListener('install', event => {
         './index.html',
         './styles/main.css',
         './scripts/main.js',
-        './img/icons/share.svg',
-        './img/icons/bank.svg',
-        './img/icons/cake.svg',
-        './img/icons/cart.svg',
-        './img/icons/pharmacy.svg',
-        './img/icons/run.svg',
-        './img/icons/support.svg',
-        './img/icons/send.svg',
-        './img/icons/person.svg',
-        './img/icons/location.svg'
+        './img/icons/send.svg'
       ])
         .then(() => self.skipWaiting())
         .catch(err => console.error(err));
