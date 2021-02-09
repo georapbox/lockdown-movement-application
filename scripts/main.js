830daf31ca0eb9a214e3a7b5694b9f4d732b2ac7
georapbox <georapbox@gmail.com>
2021-02-09 17:58:35 +0200

    add basic speech recognition

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,11 @@
 (function () {
   'use strict';
 
+  var firstLoadTimestamp = Date.now();
   var hostname = window.location.hostname;
   var isLocalEnv = hostname === '127.0.0.1' || hostname === 'localhost';
   var REPOSITORY = 'lockdown-movement-application'; // Github repository name, also the directory name when hosted as Github page
   var serviceWorkerPath = isLocalEnv ? '/sw.js' : '/' + REPOSITORY + '/sw.js';
-
-  if('serviceWorker' in navigator && !isLocalEnv) {
-    navigator.serviceWorker.register(serviceWorkerPath).catch(function (err) {
-      console.error(err);
-    });
-  }
-
   var SMS_NUMBER = 13033;
   var shareButton = document.getElementById('share-btn');
   var themeSliderEl = document.getElementById('theme-slider');
@@ -23,6 +17,24 @@
   var isAndroid = /android/i.test(userAgent);
   var isIOS = /iPad|iPhone/.test(userAgent);
   var theme = localStorage.getItem('theme') || 'theme-light';
+  var speechRecognition;
+
+  var logger = {
+    info: function () {
+      if (!isLocalEnv) return;
+      return console.log.apply(void 0, arguments);
+    },
+    error: function error() {
+      if (!isLocalEnv) return;
+      return console.error.apply(void 0, arguments);
+    }
+  };
+
+  if('serviceWorker' in navigator && !isLocalEnv) {
+    navigator.serviceWorker.register(serviceWorkerPath).catch(function (err) {
+      console.error(err);
+    });
+  }
 
   function setTheme(themeName) {
     theme = themeName;
@@ -32,6 +44,7 @@
       localStorage.setItem('theme', themeName);
     } catch (err) {
       // fail silently
+      logger.error('Error in setting theme');
     }
   }
 
@@ -88,6 +101,7 @@
       localStorage.setItem('address', addressInput.value);
     } catch (err) {
       // fail silently
+      logger.error('Error in saving in localStorage');
     }
   });
 
@@ -99,9 +113,11 @@
         title: document.title,
         text: document.querySelector('meta[name="description"]').content,
         url: document.location.href
-      })
-        .then(() => console.log('Successful share'))
-        .catch((error) => console.log('Error sharing', error));
+      }).then(function () {
+        logger.info('Successful share');
+      }).catch(function (err) {
+        logger.error('Error sharing', err);
+      });
     }, false);
   }
 
@@ -118,4 +134,53 @@
   }
 
   themeSliderEl.addEventListener('change', toggleTheme);
+
+  try {
+    speechRecognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+    speechRecognition.lang = 'el';
+    speechRecognition.interimResults = false;
+    speechRecognition.continuous = false;
+    speechRecognition.maxAlternatives = 5;
+    speechRecognition.start();
+
+    speechRecognition.onresult = function onRecognitionResult(evt) {
+      console.log(evt);
+      var found = false;
+      var resultsList = evt.results[0];
+      var mapReasonValueToSpeechValues = {
+        1: ['αποστολή 1', 'αποστολή ένα', '1 αποστολή', 'ένα αποστολή', 'send 1', 'send one'],
+        2: ['αποστολή 2', 'αποστολή δύο', '2 αποστολή', 'δύο αποστολή', 'send 2', 'send two'],
+        3: ['αποστολή 3', 'αποστολή τρία', '3 αποστολή', 'τρία αποστολή', 'send 3', 'send three'],
+        4: ['αποστολή 4', 'αποστολή τέσσερα', '4 αποστολή', 'τέσσερα αποστολή', 'send 4', 'send four'],
+        5: ['αποστολή 5', 'αποστολή πέντε', '5 αποστολή', 'πέντε αποστολή', 'send 5', 'send five'],
+        6: ['αποστολή 6', 'αποστολή έξι', '6 αποστολή', 'έξι αποστολή', 'send 6', 'send six']
+      };
+
+      for (var i = 0; i < resultsList.length; i++) {
+        logger.info(resultsList[i]);
+
+        for (var key = 1; key <= Object.keys(mapReasonValueToSpeechValues).length; key++) {
+          if (mapReasonValueToSpeechValues[key].indexOf(resultsList[i].transcript.toLowerCase()) > -1) {
+            reasonRadios.value = key;
+            document.querySelector('[type="submit"]').click();
+            found = true;
+            break;
+          }
+        }
+
+        if (found) {
+          break;
+        }
+      }
+    };
+
+    speechRecognition.onend = function onRecognitionEnd() {
+      if (Date.now() - firstLoadTimestamp < 5 * 60 * 1000) { // Restart for 5 minutes
+        speechRecognition.start();
+      }
+    };
+  } catch (err) {
+    logger.error('Speech recognition API might not be supported.');
+    logger.error(err);
+  }
 }());
